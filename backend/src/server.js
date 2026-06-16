5f2a062b66e004ad7ff2ef93ddb1d67630e95b0f
Tran Khang <n23dccn028@student.ptithcm.edu.vn>
2026-06-16 18:27:16 +0700

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import path from "path";
 
 import userRoutes from "./routes/userRouters.js";
 import driverRoutes from "./routes/driverRouters.js";
@@ -14,6 +15,8 @@ import authRoutes from "./routes/authRouters.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/drivers", driverRoutes);
@@ -24,8 +27,18 @@ app.use("/api/driver-locations", driverLocationRoutes);
 app.use("/api/incidents", incidentRoutes);
 app.use("/api/auth", authRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist", "index.html")
+    );
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, "127.0.0.1", () => {
-    console.log(`Server đang chạy ở http://127.0.0.1:${PORT}`);
+    console.log(`Server đang chạy ở http://localhost:${PORT}`);
   });
 });
